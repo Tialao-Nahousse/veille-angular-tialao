08257f69f1e95e56b1877080468b3ea8e8ea18c4
Tialao Nahoussé <tialaonahousse@gmail.com>
2021-08-17 17:59:30 +0000

    up

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -10,7 +10,7 @@ export class ApiService {
   constructor(private http : HttpClient) { }
 
   postEmploye(data : any){
-    return this.http.post<any>("http://localhost:3000/posts", data)
+    return this.http.post<any>("https://tialao-angular-crud.herokuapp.com:8080/posts", data)
     .pipe(map((res:any)=>{
       return res;
     }))
