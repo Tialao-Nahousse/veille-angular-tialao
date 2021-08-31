7b5597124e214b5296fda2f7393a5f195181e407
Tialao Nahoussé <tialaonahousse@gmail.com>
2021-08-31 09:29:22 +0000

    update api link

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -10,28 +10,28 @@ export class ApiService {
   constructor(private http : HttpClient) { }
 
   postEmploye(data : any){
-    return this.http.post<any>("http://localhost:3000/posts", data)
+    return this.http.post<any>("https://tialao-angular-crud.herokuapp.com/posts", data)
     .pipe(map((res:any)=>{
       return res;
     }))
   }
 
   getEmploye(){
-    return this.http.get<any>("http://localhost:3000/posts")
+    return this.http.get<any>("https://tialao-angular-crud.herokuapp.com/posts")
     .pipe(map((res:any)=>{
       return res;
     }))
   }
 
   updateEmploye(data : any,id: number){
-    return this.http.put<any>("http://localhost:3000/posts/"+id,data)
+    return this.http.put<any>("https://tialao-angular-crud.herokuapp.com/posts/"+id,data)
     .pipe(map((res:any)=>{
       return res;
     }))
   }
 
   deleteEmployee(id : number){
-    return this.http.delete<any>("http://localhost:3000/posts/"+id)
+    return this.http.delete<any>("https://tialao-angular-crud.herokuapp.com/posts/"+id)
     .pipe(map((res:any)=>{
       return res;
     }))
